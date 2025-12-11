d1d985cd5dd98daa404373d6d671aaf8e4ce2d39
FIADANRARA <aliffiahilvasalsa07@student.uns.ac.id>
2025-12-11 21:51:39 +0700

    Update kalkulator_akg.py

--- Streamlit_AKG/kalkulator_akg.py
+++ Streamlit_AKG/kalkulator_akg.py
@@ -765,5 +765,3 @@ with tab_metode:
     st.markdown("""
     **Penting:** Meskipun metode ini sangat akurat di antara titik-titik data (interpolasi), metode ini mungkin kurang akurat jika digunakan untuk memprediksi di luar rentang data acuan (ekstrapolasi).
     """)
-
-
